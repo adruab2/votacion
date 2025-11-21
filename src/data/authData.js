83334a7fa5f92cf8ad969d8e8aa0acb4c0267047
adruab2 <cardenas535salinas@gmail.com>
2025-11-20 20:53:18 -0500

    Añadimos una database

---
+++ src/data/authData.js
@@ -0,0 +1,14 @@
+export const admins = [
+  {
+    email: "admin@example.com",
+    password: "admin123",
+    securityCode: "ABC123"
+  }
+];
+
+export const users = [
+  {
+    dni: "12345678",
+    password: "user123"
+  }
+];
